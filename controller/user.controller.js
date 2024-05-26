dba5fa94fc9fee0f63a2b99f904bd09e77756e00
Leejeongwoo <juny1alex@gmail.com>
2024-05-26 20:12:19 +0900

    loginWithEmail error handling

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -7,7 +7,7 @@ userController.createUser = async (req, res) => {
   try {
     const { email, name, password } = req.body;
     const user = await User.findOne({ email: email });
-
+    console.log(email, name, password);
     if (user) {
       throw new Error("이미 가입이 된 유저 입니다");
     }
@@ -35,7 +35,7 @@ userController.loginWithEmail = async (req, res) => {
     }
     throw new Error("아이디 또는 비밀번호가 일치하지 않습니다");
   } catch (error) {
-    res.status(400).json({ status: "fail", error });
+    res.status(400).json({ status: "fail", message: error.message });
   }
 };
 
